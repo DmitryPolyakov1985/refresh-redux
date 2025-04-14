0ad2cba8b99276093df06c66a827a8a8f3847b4c
Dmitry <dmitry@smilecdr.com>
2025-04-14 13:53:01 -0600

    first commit

---
+++ src/store.js
@@ -0,0 +1,39 @@
+const initialState = {
+  balance: 0,
+  loan: 0,
+  loanPurpose: "",
+};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case "account/deposit":
+      return {
+        ...state,
+        balance: state.balance + action.payload,
+      };
+
+    case "account/withdraw":
+      return {
+        ...state,
+        balance: state.balance - action.payload,
+      };
+
+    case "account/requestLoan":
+      if (state.load > 0) return state;
+      return {
+        ...state,
+        load: action.payload,
+      };
+
+    case "account/payLoan":
+      return {
+        ...state,
+        loan: 0,
+        loadPurpose: "",
+        balance: state.balance - loan,
+      };
+
+    default:
+      return state;
+  }
+}

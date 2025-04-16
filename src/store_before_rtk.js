7937358d12264fe33c0f82ed8eef928fda1e0b12
Dmitry <dmitry@smilecdr.com>
2025-04-15 22:51:43 -0600

    moved to rtk

---
+++ src/store_before_rtk.js
@@ -0,0 +1,17 @@
+import { thunk } from "redux-thunk";
+import accountReducer from "./features/accounts/accountSlice";
+import customerReducer from "./features/customers/customerSlice";
+import { composeWithDevTools } from "redux-devtools-extension";
+const { createStore, combineReducers, applyMiddleware } = require("redux");
+
+const rootReducer = combineReducers({
+  account: accountReducer,
+  customer: customerReducer,
+});
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
+export default store;

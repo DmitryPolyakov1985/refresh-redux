7b9f01cb89d8ff12bf85ec62f0f044c03d8c66d2
Dmitry <dmitry@smilecdr.com>
2025-04-15 21:53:11 -0600

    added thunk

--- src/store.js
+++ src/store.js
@@ -1,12 +1,17 @@
+import { thunk } from "redux-thunk";
 import accountReducer from "./features/accounts/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
-const { createStore, combineReducers } = require("redux");
+import { composeWithDevTools } from "redux-devtools-extension";
+const { createStore, combineReducers, applyMiddleware } = require("redux");
 
 const rootReducer = combineReducers({
   account: accountReducer,
   customer: customerReducer,
 });
 
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 export default store;
